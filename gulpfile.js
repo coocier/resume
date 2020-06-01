f53fef4fcfd5cefe77490cfc857f2669bf4aed5c
Xuke.Wang <xuke.wang@geely.com>
2020-06-01 16:40:11 +0800

    node 升级后的配置修改

--- gulpfile.js
+++ gulpfile.js
@@ -58,12 +58,12 @@ function less2css(srcPath, destPath, debug) {
 }
 
 gulp.task('less', function() {
-  less2css('./src/less/questions.less', './dist/questions/');
+  // less2css('./src/less/questions.less', './dist/questions/');
   less2css('./src/less/index.less', './dist/');
 });
 
 gulp.task('less-debug', function() {
-  less2css('./src/less/questions.less', './dist/questions/', true);
+  // less2css('./src/less/questions.less', './dist/questions/', true);
   less2css('./src/less/index.less', './dist/', true);
 });
 
@@ -73,34 +73,38 @@ gulp.task('static', function() {
     .pipe(gulp.dest('./dist/static/'));
 });
 
-/****************** Watch ****************/
-gulp.task('watch', ['server'], function() {
-  gulp.watch(['./src/**/*.jade', './resume.json', './i18n/**/*.js'],
-             ['jade']);
-  gulp.watch('./static/**/*', ['static']);
-  gulp.watch('./src/**/*.less', ['less-debug']);
-  gulp.watch('./dist/**/*', function() {
-    server.notify.apply(server, arguments);
-  });
-});
-
 /****************** Build ****************/
-gulp.task('build', ['jade', 'less-debug', 'static']);
-gulp.task('build-for-deploy', ['jade', 'less', 'static']);
+gulp.task('build', gulp.series('jade', 'less-debug', 'static'));
+gulp.task('build-for-deploy', gulp.series('jade', 'less', 'static'));
 
 /****************** Server ****************/
 gulp.task('serve', function () {
   server.start();
 });
 
-gulp.task('server', ['build', 'serve']);
-gulp.task('preview', ['build-for-deploy', 'serve']);
+gulp.task('server', gulp.series('build', 'serve'));
+gulp.task('preview', gulp.series('build-for-deploy', 'serve'));
 
-/****************** Deploy ****************/
-gulp.task('deploy', ['build-for-deploy'], function() {
-  return gulp.src('./dist/**/*')
-    .pipe(plugins.ghPages());
+/****************** Watch ****************/
+gulp.task('watch', function (done) {
+  gulp.series('server', function() {
+    gulp.watch(['./src/**/*.jade', './resume.json', './i18n/**/*.js'],
+        ['jade']);
+    gulp.watch('./static/**/*', ['static']);
+    gulp.watch('./src/**/*.less', ['less-debug']);
+    gulp.watch('./dist/**/*', function() {
+      server.notify.apply(server, arguments);
+    });
+    done();
+  });
+
 });
 
+/****************** Deploy ****************/
+gulp.task('deploy', gulp.series('build-for-deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(plugins.ghPages());
+}));
+
 /****************** Default ****************/
-gulp.task('default', ['server', 'watch']);
+gulp.task('default', gulp.series('server', 'watch'));
